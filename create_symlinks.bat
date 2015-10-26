ecfcf5b688146296758d294ae0c53600b3fc68e8
Robert Herbig <sheltim@gmail.com>
2015-10-25 22:20:45 -0400

    Fixed error in batch file
    Moved the goto:eof line so that the batch file because after the calls,
    it then tried to run the del and mklink lines (using '' for %~1). This
    produced an error but had no negative effect.

--- create_symlinks.bat
+++ create_symlinks.bat
@@ -1,12 +1,12 @@
-Set _Source=C:\[CHANGE THIS]
+Set _Source=.
 Set _Destination=C:\[CHANGE THIS]
 
 call:symlink combat-trainer
 call:symlink combat-trainer-setup
 call:symlink crossing-training
 call:symlink crossing-training-setup
+goto:eof
 
 :symlink
 del %_Destination%\%~1.lic
 mklink /H %_Destination%\%~1.lic %_Source%\%~1.lic
-goto:eof

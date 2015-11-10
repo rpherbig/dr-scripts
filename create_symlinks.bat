0f2fbae5534709eea5e4b23419cf1986a6582bba
Robert Herbig <sheltim@gmail.com>
2015-11-10 09:38:31 -0500

    Made create_symlinks.bat act upon all *.lic files
    The batch file will now create a symlink ("hardlink" in Windows-speak)
    for all .lic files in the repo.
    
    There is no longer a need to update this batch file as new scripts are
    added to the repo.

--- create_symlinks.bat
+++ create_symlinks.bat
@@ -1,16 +1,14 @@
+@ECHO OFF
 Set _Destination=C:\[CHANGE THIS]
 
-call:symlink combat-trainer
-call:symlink combat-trainer-setup
-call:symlink common
-call:symlink crossing-repair
-call:symlink crossing-repair-setup
-call:symlink crossing-training
-call:symlink crossing-training-setup
-call:symlink events
-call:symlink smartlisten
+FOR /f %%a IN (
+ 'dir /b *.lic" '
+ ) DO (
+ call:symlink %%a
+)
+
 goto:eof
 
 :symlink
-del %_Destination%\%~1.lic
-mklink /H %_Destination%\%~1.lic .\%~1.lic
+del %_Destination%\%~1
+mklink /H %_Destination%\%~1 .\%~1

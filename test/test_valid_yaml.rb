f84295b8360a443090da8a9e8b12bc905b105515
Robert Herbig <sheltim@gmail.com>
2016-02-20 11:21:55 -0500

    Add assert to TestValidYaml

--- test/test_valid_yaml.rb
+++ test/test_valid_yaml.rb
@@ -3,8 +3,11 @@ require 'yaml'
 
 class TestValidYaml < Minitest::Test
   def test_all_yaml_in_profiles
+    count = 0
     Dir.glob('profiles/**/*.yaml').each do |file|
+      count += 1
       YAML.load_file file
     end
+    refute_equal count, 0, "Expected to load and parse at least one YAML file"
   end
 end

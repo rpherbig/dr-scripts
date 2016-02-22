39e4b7c7f08624e164d4d5787674b60c156af5ed
Robert Uhl <rcuhljr@gmail.com>
2016-02-21 20:51:16 -0500

    Looking into testing options

---
+++ test/test_common.rb
@@ -0,0 +1,150 @@
+require 'minitest/autorun'
+
+module UserVars
+  def UserVars.trash_storage=(val)
+  end
+end
+
+module Harness2
+  class Script
+    def gets?
+      ''
+    end
+  end
+
+  def script
+    Script.new
+  end
+
+  def pause(*); end
+
+  def echo(message)
+    # puts message
+  end
+
+  def checkdead
+    $dead || false
+  end
+
+  def checkhealth
+    $health || 100
+  end
+
+  def fput(message)
+    sent_messages << message
+  end
+
+  def put(message)
+    sent_messages << message
+  end
+
+  def sent_messages
+    $sent_messages ||= Queue.new
+  end
+
+  def health=(health)
+    $health = health
+  end
+
+  def dead=(dead)
+    $dead = dead
+  end
+
+  def waitrt?; end
+
+  def get?
+    $history ? $history.shift : nil
+  end
+
+  def clear; end
+
+  def run_script_with_proc(script, test)
+    Thread.abort_on_exception=true
+    thread = Thread.new do
+      script = "#{script}.lic" unless script.end_with?('.lic')
+      load script
+      test.call
+    end
+    $threads ||= []
+    $threads << thread
+    thread
+  end
+
+  def assert_sends_messages(expected_messages)
+    expected_messages = expected_messages.clone
+
+    consumer = Thread.new do
+      loop do
+        message = sent_messages.pop
+
+        if message == expected_messages.first
+          expected_messages.pop
+          if expected_messages.any?
+            next
+          else
+            break
+          end
+        end
+
+        sleep 0.1
+      end
+    end
+
+    10.times do |_|
+      sleep 0.1 if consumer.alive?
+    end
+
+    $threads.last.kill
+    assert_empty expected_messages, "Expected script to send #{expected_messages}"
+  end
+end
+
+include Harness2
+include UserVars
+
+class TestCommon < Minitest::Test
+  def setup
+    sent_messages.clear
+  end
+
+  def test_bput_sends_message
+    run_script_with_proc('common', proc {
+      DRC.bput('a test message')
+    })
+
+    assert_sends_messages ['a test message']
+  end
+
+  def test_bput_returns_a_match
+    $history = ['A result string']
+
+    run_script_with_proc('common', proc {
+      assert_equal 'result', DRC.bput('a test message', 'result')
+    })
+
+  end
+
+  def test_bput_returns_a_match_with_other_data
+    $history = [nil,'not the correct string', '', 'A result string']
+
+    run_script_with_proc('common', proc {
+      assert_equal 'result', DRC.bput('a test message', 'result')
+    })
+
+  end
+
+  # def test_bput_returns_error_code_after_timeout
+  #   $history = [nil,'not the correct string', '']
+  #   $time_stubs = [0,1,2,3,15]
+
+  #   Time.stub :now, proc {$time_stubs.shift} do
+  #     run_script_with_proc('common', proc {
+  #       assert_equal true, false
+  #     })
+
+  #     assert_sends_messages ['a test message']
+  #   end
+
+  # end
+
+end

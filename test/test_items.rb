7d2a48bb845a199f89415044f96ff63132828027
Robert Uhl <rcuhljr@gmail.com>
2016-04-01 19:04:51 -0400

    Remove long description format

--- test/test_items.rb
+++ test/test_items.rb
@@ -6,33 +6,23 @@ class TestItems < Minitest::Test
     load('common.lic')
   end
 
-  def test_short_name_without_short_gear
-    actual = DRC::Item.new(name: 'sword')
-    assert_equal nil, actual.short_name
-  end
-
   def test_short_name_with_no_adjective
-    actual = DRC::Item.new(name: 'sword', short_gear: true)
+    actual = DRC::Item.new(name: 'sword')
     assert_equal 'sword', actual.short_name
   end
 
   def test_short_name
-    actual = DRC::Item.new(name: 'sword', adjective: 'steel', short_gear: true)
+    actual = DRC::Item.new(name: 'sword', adjective: 'steel')
     assert_equal 'steel.sword', actual.short_name
   end
 
-  def test_short_regex_without_short_gear
-    actual = DRC::Item.new(name: 'sword')
-    assert_nil actual.short_regex
-  end
-
   def test_short_regex_with_no_adjective
-    actual = DRC::Item.new(name: 'sword', short_gear: true)
+    actual = DRC::Item.new(name: 'sword')
     assert_equal(/\b#{'sword'}/i, actual.short_regex)
   end
 
   def test_short_regex
-    actual = DRC::Item.new(name: 'sword', adjective: 'steel', short_gear: true)
+    actual = DRC::Item.new(name: 'sword', adjective: 'steel')
     assert_equal(/#{'steel'}.*\b#{'sword'}/i, actual.short_regex)
   end
 end

9c1127d3e5df97e725a402deb7eb99b487af5ce6
Robert Herbig <sheltim@gmail.com>
2016-04-30 09:53:08 -0400

    Rubocop

--- test/test_valid_yaml.rb
+++ test/test_valid_yaml.rb
@@ -10,18 +10,18 @@ class TestValidYaml < Minitest::Test
     end
     refute_equal count, 0, 'Expected to load and parse at least one YAML file'
   end
-  
+
   def test_all_settings_in_base_yaml
     base = YAML.load_file('./profiles/base.yaml').keys
 
     nonbase = Dir.glob('./profiles/*.yaml')
-      .delete_if { |item| item == '.' }
-      .delete_if { |item| item == '..' }
-      .delete_if { |item| item.include?('base') }
-      .map { |item| YAML.load_file(item) }
-      .each_with_object({}) { |item, obj| obj.merge!(item) }
-      .keys
-    
+                 .delete_if { |item| item == '.' }
+                 .delete_if { |item| item == '..' }
+                 .delete_if { |item| item.include?('base') }
+                 .map { |item| YAML.load_file(item) }
+                 .each_with_object({}) { |item, obj| obj.merge!(item) }
+                 .keys
+
     refute_equal base.count, 0, 'Expected to find at least one setting in base.yaml'
     refute_equal nonbase.count, 0, 'Expected to find at least one setting in a non-base yaml'
     assert_equal nonbase - base, [], 'Expected all settings to be in base.yaml'

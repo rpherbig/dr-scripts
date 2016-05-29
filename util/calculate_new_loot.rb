db0bb407e08732828724b3942403985443f4cf83
Keith Marcum <marcum.keith@gmail.com>
2016-05-29 08:57:55 -0400

    Update Moose and add script for lootables

---
+++ util/calculate_new_loot.rb
@@ -0,0 +1,36 @@
+require 'yaml'
+
+base = YAML.load_file('./profiles/base.yaml')['lootables']
+
+nonbase = Dir.glob('./profiles/*.yaml')
+             .delete_if { |item| item == '.' }
+             .delete_if { |item| item == '..' }
+             .delete_if { |item| item.include?('base') }
+             .map { |item| [item, YAML.load_file(item)] }
+
+nonbase.each do |pair|
+  filename, nonbase = pair
+  nonbase_lootables = nonbase['lootables']
+  next unless nonbase_lootables
+
+  additional = nonbase_lootables - base
+  excluded = base - nonbase_lootables
+
+  next unless [additional, excluded].any?(&:any?)
+
+  puts "append the following to #{filename} and remove the lootables key:"
+
+  if additional.any?
+    puts "loot_additions:"
+    additional.each do |addition|
+      puts "- #{addition}"
+    end
+  end
+
+  if excluded.any?
+    puts "loot_subtractions:"
+    excluded.each do |exclusion|
+      puts "- #{exclusion}"
+    end
+  end
+end

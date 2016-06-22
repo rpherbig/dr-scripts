9f5cbade4b16ffb0011336d31e429233f138f917
Robert Uhl <uhlrc@yahoo.com>
2016-06-22 16:48:34 -0400

    rubocop

--- test/test_validate.rb
+++ test/test_validate.rb
@@ -35,21 +35,21 @@ class TestValidate < Minitest::Test
                      {
                        adjective: 'chain', name: 'balaclava'
                      },
-        {
-          adjective: 'mail', name: 'gloves'
-        },
-        {
-          adjective: 'small', name: 'shield'
-        }
+                     {
+                       adjective: 'mail', name: 'gloves'
+                     },
+                     {
+                       adjective: 'small', name: 'shield'
+                     }
                    ],
-                    'gear_sets' => {
-        'standard' => [
-          'chain balaclava',
-          'mail gloves',
-          'small shield',
-          'padded hauberk'
-        ]
-      })
+                   'gear_sets' => {
+                     'standard' => [
+                       'chain balaclava',
+                       'mail gloves',
+                       'small shield',
+                       'padded hauberk'
+                     ]
+                   })
 
     load('validate.lic')
 
@@ -60,14 +60,14 @@ class TestValidate < Minitest::Test
     setup_settings('gear' => [
                      {
                        adjective: 'robe', name: 'sword'
-                     },
+                     }
                    ],
-                    'weapon_training' => {
-        'Large Edged' => 'robe sword',
-        'Twohanded Edged' => 'moonblade',
-        'Small Blunt' => 'missing item'
-      },
-      'summoned_weapons' => [{'name' => 'Twohanded Edged'}
+                   'weapon_training' => {
+                     'Large Edged' => 'robe sword',
+                     'Twohanded Edged' => 'moonblade',
+                     'Small Blunt' => 'missing item'
+                   },
+                   'summoned_weapons' => [{ 'name' => 'Twohanded Edged' }
       ])
 
     load('validate.lic')
@@ -75,5 +75,4 @@ class TestValidate < Minitest::Test
     assert $error_msgs.grep(/Small Blunt: missing item/), 'Expected warning for missing item'
     assert $error_msgs.grep(/moonblade/).empty?, "Expected no warning for summoned gear, found #{$error_msgs.grep(/moonblade/)}"
   end
-
 end

ac7eae0c03a514412cab2593ec6cd3ac84103e8f
Robert Uhl <rcuhljr@gmail.com>
2016-06-25 15:08:02 -0400

    rubocop fixes

--- util/update_stealing.rb
+++ util/update_stealing.rb
@@ -6,5 +6,5 @@ firebase = Firebase::Client.new('https://dr-scripts.firebaseio.com/')
 stealing = firebase.get('stealing-options').body
 
 stealing_data = YAML.load_file('./profiles/base-stealing.yaml')
-stealing_data[:stealing_options].each { |data| data.merge!( stealing.find{|x| x['id'] == data['id']} )  }
-File.open('./profiles/base-stealing.yaml', 'w'){ |file| file.print(stealing_data.to_yaml) }
+stealing_data[:stealing_options].each { |data| data.merge!(stealing.find { |x| x['id'] == data['id'] }) }
+File.open('./profiles/base-stealing.yaml', 'w') { |file| file.print(stealing_data.to_yaml) }

63ae465aa6a00b34cefb3fcd86dfc0070db1a7a9
Robert Uhl <rcuhljr@gmail.com>
2016-06-25 02:04:21 -0400

    update script

--- util/update_stealing.rb
+++ util/update_stealing.rb
@@ -7,6 +7,4 @@ stealing = firebase.get('stealing-options').body
 
 stealing_data = YAML.load_file('./profiles/base-stealing.yaml')
 stealing_data[:stealing_options].each { |data| data.merge!( stealing.find{|x| x['id'] == data['id']} )  }
-File.open(stealing_path, 'w') { |file| file.print(stealing_data.to_yaml) }
-
 File.open('./profiles/base-stealing.yaml', 'w'){ |file| file.print(stealing_data.to_yaml) }

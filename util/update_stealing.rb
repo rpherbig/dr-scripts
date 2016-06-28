cc750a2bdf3db64ad8c32463d878ad23c7ef2bcb
Robert Uhl <rcuhljr@gmail.com>
2016-06-27 23:18:19 -0400

    Get new items

--- util/update_stealing.rb
+++ util/update_stealing.rb
@@ -6,5 +6,12 @@ firebase = Firebase::Client.new('https://dr-scripts.firebaseio.com/')
 stealing = firebase.get('stealing-options').body
 
 stealing_data = YAML.load_file('./profiles/base-stealing.yaml')
-stealing_data[:stealing_options].each { |data| data.merge!(stealing.find { |x| x['id'] == data['id'] }) }
+stealing_data[:stealing_options].each do |data|
+  update = stealing.find { |x| x['id'] == data['id'] }
+  data.merge!(update)
+  stealing.delete(update)
+end
+
+stealing.each{|x| stealing_data[:stealing_options] << x }
+
 File.open('./profiles/base-stealing.yaml', 'w') { |file| file.print(stealing_data.to_yaml) }

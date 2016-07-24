34512cd31dd024bd980e487d9e32f338684e3651
Robert Herbig <sheltim@gmail.com>
2016-07-23 20:21:32 -0400

    Remove bad item from thieving data

--- util/update_stealing.rb
+++ util/update_stealing.rb
@@ -7,7 +7,7 @@ stealing = firebase.get('stealing-options').body
 
 stealing_data = YAML.load_file('./profiles/base-stealing.yaml')
 stealing_data[:stealing_options].each do |data|
-  update = stealing.find { |x| x['id'] == data['id'] }
+  update = stealing.compact.find { |x| x['id'] == data['id'] }
   data.merge!(update)
   stealing.delete(update)
 end

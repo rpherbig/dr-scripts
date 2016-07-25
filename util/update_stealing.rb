c984b7753397102c440c891fe622fbc351ac2dc8
Robert Herbig <sheltim@gmail.com>
2016-07-24 21:22:33 -0400

    Fix update_stealing.rb

--- util/update_stealing.rb
+++ util/update_stealing.rb
@@ -6,7 +6,7 @@ firebase = Firebase::Client.new('https://dr-scripts.firebaseio.com/')
 stealing = firebase.get('stealing-options').body
 
 stealing_data = YAML.load_file('./profiles/base-stealing.yaml')
-stealing_data[:stealing_options].each do |data|
+stealing_data[:stealing_options].compact.each do |data|
   update = stealing.compact.find { |x| x['id'] == data['id'] }
   data.merge!(update)
   stealing.delete(update)

f9e6ca7db80b83b5ceadb831296acb6b44e3428b
Jonas Andersson <jonas@andersson.io>
2018-05-11 14:20:17 +0200

    remove problematic tests

--- test/test_drinfomon.rb
+++ test/test_drinfomon.rb
@@ -41,30 +41,10 @@ class TestDrinfomon < Minitest::Test
     sent_messages.clear
   end
 
-  def test_sends_info_on_startup_when_alive
-    self.dead = false
-    $history = ["Name: Navigator Dijkstra Bellman-Ford of Elanthia   Race: Elothean   Guild: Trader",
-                'Gender: Male   Age: 31   Circle: 57',
-                'Wealth: 0']
-    run_script('drinfomon.lic')
-    assert_sends_messages(['exp all', 'info'])
-  end
-
   def test_skips_info_on_startup_when_dead
     self.dead = true
     run_script('drinfomon.lic')
     assert_sends_messages(['exp all'])
-  end
-
-  def test_sends_info_after_departing
-    self.dead = true
-    run_script('drinfomon.lic')
-    sent_messages.clear
-    Timecop.scale(60)
-    self.dead = false
-    sleep(0.3)
-    Timecop.return()
-    assert_sends_messages(['info'])
   end
 
   def test_drstats_parses_info_correctly
@@ -105,13 +85,4 @@ class TestDrinfomon < Minitest::Test
     assert_equal('None', DRStats.encumbrance)
   end
 
-  def test_sends_exp_all_after_exp_conversion
-    self.dead = false
-    $history = ["Name: Navigator Dijkstra Bellman-Ford of Elanthia   Race: Elothean   Guild: Trader",
-                'Gender: Male   Age: 31   Circle: 57',
-                'Wealth: 0',
-                "Log-on system converted 100% of your character's field experience into earned rank."]
-    run_script('drinfomon')
-    assert_sends_messages(['exp all', 'info', 'exp all'])
-  end
 end

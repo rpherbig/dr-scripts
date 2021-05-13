2b6e70195632bab24194dcad45d3840d61b47585
KatoakDR <68095633+KatoakDR@users.noreply.github.com>
2021-05-12 23:10:31 -0500

    update tests

--- test/test_find.rb
+++ test/test_find.rb
@@ -19,7 +19,11 @@ class TestFind < Minitest::Test
   end
 
   def assert_find_npc
-    proc { nil }
+    proc { refute($displayed_messages.any? { |message| message.include?('Failed to find an NPC') }) }
+  end
+
+  def assert_not_find_npc
+    proc { assert($displayed_messages.any? { |message| message.include?('Failed to find an NPC') }) }
   end
 
   def run_find(messages, npc, fake_drc, fake_drct, assertions = [])
@@ -46,7 +50,7 @@ class TestFind < Minitest::Test
     end)
   end
 
-  def test_find_npc
+  def test_find_npc_one_word
     messages = []
 
     npc = 'beggar'
@@ -62,7 +66,47 @@ class TestFind < Minitest::Test
     end
 
     run_find(messages, npc, fake_drc, fake_drct, [
+      assert_find_npc
+    ])
+  end
 
+  def test_find_npc_full_name
+    messages = []
+
+    npc = 'old blind beggar'
+
+    fake_drc = Minitest::Mock.new
+
+    fake_drct = Minitest::Mock.new
+    [793, 792, 791, 789].each do |room_id|
+      fake_drct.expect(:walk_to, true, [room_id])
+    end
+    fake_drct.expect(:walk_to, true) do |room_id|
+      DRRoom.npcs = ['old blind beggar']
+    end
+
+    run_find(messages, npc, fake_drc, fake_drct, [
+      assert_find_npc
+    ])
+  end
+
+  def test_not_find_npc
+    messages = []
+
+    npc = 'beggar'
+
+    fake_drc = Minitest::Mock.new
+
+    fake_drct = Minitest::Mock.new
+    [793, 792, 791, 789].each do |room_id|
+      fake_drct.expect(:walk_to, true, [room_id])
+    end
+    fake_drct.expect(:walk_to, true) do |room_id|
+      DRRoom.npcs = ['street urchin']
+    end
+
+    run_find(messages, npc, fake_drc, fake_drct, [
+      assert_not_find_npc
     ])
   end
 

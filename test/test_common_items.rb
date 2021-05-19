6ae799204db5784ed79db3a427212394018ebda8
KatoakDR <68095633+KatoakDR@users.noreply.github.com>
2021-05-18 22:26:34 -0500

    get_item? tests

--- test/test_common_items.rb
+++ test/test_common_items.rb
@@ -5,6 +5,7 @@ load 'test/test_harness.rb'
 include Harness
 
 class TestDRCI < Minitest::Test
+
   def setup
     reset_data
     $history.clear
@@ -15,6 +16,133 @@ class TestDRCI < Minitest::Test
 
   def teardown
     @test.join if @test
+  end
+
+  #########################################
+  # RUN DRCI COMMAND
+  #########################################
+
+  def assert_result
+    proc { |result| assert(result) }
+  end
+
+  def refute_result
+    proc { |result| refute(result) }
+  end
+
+  def run_drci_command(messages, command, args, assertions = [])
+    @test = run_script_with_proc(['common', 'common-items'], proc do
+      # Setup
+      $server_buffer = messages.dup
+      $history = $server_buffer.dup
+
+      # Test
+      result = DRCI.send(command, *args)
+
+      # Assert
+      assertions = [assertions] unless assertions.is_a?(Array)
+      assertions.each { |assertion| assertion.call(result) }
+    end)
+  end
+
+  #########################################
+  # GET ITEM
+  #########################################
+
+  def test_get_item_success
+    command = 'get_item?'
+    fixtures = [
+      {
+        :message => "You get a {{item}} from inside your hitman's backpack.",
+        :item => "sanowret crystal"
+      },
+      {
+        :message => "You pick up a {{item}}.",
+        :item => "drake-fang arrow"
+      },
+      {
+        :message => "You pluck an emaciated pet {{item}} from a coarse burlap sack, the creature yawning as you wake it up.",
+        :item => "rat"
+      },
+      {
+        :message => "You deftly remove the {{item}} from your sash.",
+        :item => "spiky barb"
+      },
+      {
+        :message => "You fade in for a moment as you pick up a {{item}} fit with a polished flamewood handle.",
+        :item => "dainty cowbell"
+      },
+      {
+        :message => "You fade in for a moment as you get a small rust-colored {{item}} from a black gem pouch.",
+        :item => "andalusite"
+      }
+    ]
+    fixtures.each do |fixture|
+      run_drci_command(
+        [fixture[:message].gsub('{{item}}', fixture[:item])],
+        command,
+        [fixture[:item]],
+        [assert_result]
+      ).join
+    end
+  end
+
+  def test_get_item_failure
+    command = 'get_item?'
+    fixtures = [
+      {
+        :message => "But that is already in your inventory.",
+        :item => "sanowret crystal"
+      },
+      {
+        :message => "You need a free hand to do that.",
+        :item => "drake-fang arrow"
+      },
+      {
+        :message => "You need a free hand to pick that up.",
+        :item => "rat"
+      },
+      {
+        :message => "Picking up a rugged {{item}} would push you over the item limit of 100.  Please reduce your inventory count before you try again.",
+        :item => "red backpack"
+      },
+      {
+        :message => "You stop as you realize the {{item}} is not yours.",
+        :item => "boar-tusk arrow"
+      },
+      {
+        :message => "Get what?",
+        :item => "anything"
+      },
+      {
+        :message => "I could not find what you were referring to.",
+        :item => "something"
+      },
+      {
+        :message => "I could not find that container.",
+        :item => "backpack"
+      },
+      {
+        :message => "What were you referring to?",
+        :item => "nothing"
+      },
+      {
+        :message => "The large limb rapidly decays away.",
+        :item => "limb"
+      },
+      {
+        :message => "A sharp pine sapling cracks and rots away along with the troll.",
+        :item => "sapling"
+      }
+    ]
+    fixtures.each do |fixture|
+      run_drci_command(
+        [fixture[:message].gsub('{{item}}', fixture[:item])],
+        command,
+        [fixture[:item]],
+        [refute_result]
+      ).join
+    end
   end
 
   #########################################

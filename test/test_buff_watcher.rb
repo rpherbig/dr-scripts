b8e8237a2d7d582b2f00fe84fb71e06d9115b73c
KatoakDR <68095633+KatoakDR@users.noreply.github.com>
2021-05-21 01:20:09 -0500

    new test for buff-watcher.lci

---
+++ test/test_buff_watcher.rb
@@ -0,0 +1,196 @@
+require_relative 'test_helper'
+require 'yaml'
+
+load 'test/test_harness.rb'
+
+include Harness
+
+class TestBuffWatcher < Minitest::Test
+
+  def setup
+    reset_data
+    load_base_settings
+  end
+
+  def teardown
+    @test.join if @test
+  end
+
+  def load_base_settings(settings = {})
+    $test_settings = OpenStruct.new(YAML.load_file('profiles/base.yaml').merge(settings))
+    YAML.load_file('profiles/base-empty.yaml')['empty_values'].each { |key, val| $test_settings[key] ||= val }
+  end
+
+  def run_buff_watcher(messages, script_args, fake_drc, fake_drci, fake_drca, on_script_start_hook, assertions = [], expected_errors = [])
+    @test = run_script_with_proc(['buff-watcher'], proc do
+      # Setup
+      $server_buffer = messages.dup
+      $history = $server_buffer.dup
+      $parsed_args = script_args.dup
+
+      mocks = [fake_drc, fake_drci, fake_drca]
+
+      original_DRC = BuffWatcher::DRC if defined?(BuffWatcher::DRC)
+      BuffWatcher.send(:remove_const, "DRC") if defined?(BuffWatcher::DRC)
+      BuffWatcher.const_set("DRC", fake_drc)
+
+      original_DRCI = BuffWatcher::DRCI if defined?(BuffWatcher::DRCI)
+      BuffWatcher.send(:remove_const, "DRCI") if defined?(BuffWatcher::DRCI)
+      BuffWatcher.const_set("DRCI", fake_drci)
+
+      original_DRCA = BuffWatcher::DRCA if defined?(BuffWatcher::DRCA)
+      BuffWatcher.send(:remove_const, "DRCA") if defined?(BuffWatcher::DRCA)
+      BuffWatcher.const_set("DRCA", fake_drca)
+
+      # Test
+      begin
+        error = nil
+        if on_script_start_hook
+          thread = Thread.new { BuffWatcher.new }
+          on_script_start_hook.call(thread)
+          thread.join
+        else
+          BuffWatcher.new
+        end
+      rescue Exception => e
+        error = e
+        unless expected_errors.include?(error.class)
+          flunk(exception_details(e, 'Unexpected error running test'))
+        end
+      end
+
+      # Restore injected dependencies
+      BuffWatcher.send(:remove_const, "DRC") if defined?(BuffWatcher::DRC)
+      BuffWatcher.const_set("DRC", original_DRC)
+
+      BuffWatcher.send(:remove_const, "DRCI") if defined?(BuffWatcher::DRCI)
+      BuffWatcher.const_set("DRCI", original_DRCI)
+
+      BuffWatcher.send(:remove_const, "DRCA") if defined?(BuffWatcher::DRCA)
+      BuffWatcher.const_set("DRCA", original_DRCA)
+
+      # Assert
+      assertions = [assertions] unless assertions.is_a?(Array)
+      mocks.each { |mock| assertions << proc { assert_mock(mock) } if mock.class == Minitest::Mock }
+      assertions.each { |assertion| assertion.call(error) }
+    end)
+  end
+
+  def test_should_exit_if_no_buff_set_defined
+    $test_settings.waggle_sets = {
+      'buffy' => [
+        'Super Power'
+      ]
+    }
+
+    script_args = {}
+
+    messages = []
+
+    fake_drc = Minitest::Mock.new
+    fake_drci = Minitest::Mock.new
+    fake_drca = Minitest::Mock.new
+
+    on_script_start_hook = nil
+
+    assertions = [
+      assert_displayed_messages_include_any(["No spells defined in buff set 'default'.  Ending script.  Double check your settings."]),
+      assert_raise_error(SystemExit, 'exit')
+    ]
+
+    expected_errors = [
+      SystemExit
+    ]
+
+    run_buff_watcher(messages, script_args, fake_drc, fake_drci, fake_drca, on_script_start_hook, assertions, expected_errors)
+  end
+
+  def test_should_not_buff_if_hidden
+    $test_settings.waggle_sets = {
+      'buffy' => [
+        'Super Power'
+      ]
+    }
+
+    script_args = {
+      'buff_set_name' => 'buffy'
+    }
+
+    messages = []
+
+    DRStats.guild = 'Thief'
+    $hidden = true
+
+    fake_drc = Minitest::Mock.new
+    fake_drci = Minitest::Mock.new
+    fake_drca = Minitest::Mock.new
+
+    on_script_start_hook = proc do |thread|
+      # Let the buff watcher's passive loop run a bit then kill the script.
+      Thread.kill(thread)
+    end
+
+    assertions = [
+      # nothing extra needed, when the mocks are verified
+      # that'll confirm that the script never got to spots
+      # that called that code
+    ]
+
+    expected_errors = []
+
+    run_buff_watcher(messages, script_args, fake_drc, fake_drci, fake_drca, on_script_start_hook, assertions, expected_errors)
+  end
+
+  def test_should_not_buff_if_invisible
+    $test_settings.waggle_sets = {
+      'buffy' => [
+        'Super Power'
+      ]
+    }
+
+    script_args = {
+      'buff_set_name' => 'buffy'
+    }
+
+    messages = []
+
+    DRStats.guild = 'Thief'
+    $invisible = true
+
+    fake_drc = Minitest::Mock.new
+    fake_drci = Minitest::Mock.new
+    fake_drca = Minitest::Mock.new
+
+    on_script_start_hook = proc do |thread|
+      # Let the buff watcher's passive loop run a bit then kill the script.
+      Thread.kill(thread)
+    end
+
+    assertions = [
+      # nothing extra needed, when the mocks are verified
+      # that'll confirm that the script never got to spots
+      # that called that code
+    ]
+
+    expected_errors = []
+
+    run_buff_watcher(messages, script_args, fake_drc, fake_drci, fake_drca, on_script_start_hook, assertions, expected_errors)
+  end
+
+  # def test_should_not_buff_if_running_no_use_scripts
+
+  # end
+
+  # def test_should_not_buff_if_inside_no_use_room
+
+  # end
+
+  # def test_should_not_buff_if_need_inner_fire
+
+  # end
+
+  # def test_should_not_buff_if_all_buffs_active
+
+  # end
+
+end
